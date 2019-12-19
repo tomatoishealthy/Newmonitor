d995114986146bb250718f6eb3c2bdb4e24214ab
wubinTRON <wubin1@tron.network>
2019-12-19 11:47:03 +0800

    fix bug

--- alerts/getnowblockalert.go
+++ alerts/getnowblockalert.go
@@ -85,9 +85,6 @@ func (g *GetNowBlockAlert) Load() {
 		len(g.Nodes))
 }
 
-e
-
-
 func callUpdateAnomaly(number string) {
 
 	client, err := sdk.NewClientWithAccessKey("default", "LTAIbEOdCXFYrP98", "wNVf3zMK6dqwxvwp2oYsq9iTBYPXq1")
@@ -250,6 +247,13 @@ func (g *GetNowBlockAlert) getNodeBlockNum(ip string, port int,
 		port)
 
 	return g.getBlockNum(q)
+}
+
+func callWhoUpdateAnomaly() {
+	DUTY := []string{"吴彬", "岳瑞鹏", "姜阳阳", "张思聪", "吴斌", "梁志彦", "孙昊宇"}
+	Phone := []string{"18903830819", "13311527723", "13810109462", "13466613212", "18515212681", "15256073545", "15901009909"}
+	who  := (time.Now().Unix() - 86400 * 4) / 86400 / 7 % int64(len(DUTY))
+	callUpdateAnomaly(Phone[who])
 }
 
 func (g *GetNowBlockAlert) GetMaxBlockNum(
